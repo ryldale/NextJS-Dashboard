3c2f1ab822d47629fc74e3188b3ba42e88672d2e
rylgel <official.torresryldale@gmail.com>
2026-03-11 10:25:48 +0800

    sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
 import "@/app/ui/global.css";
 import { inter } from "./ui/fonts";
 import { Metadata } from "next";
- 
+
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-js-dashboard-delta-gold.vercel.app/'),
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-js-dashboard-delta-gold.vercel.app/"),
+  openGraph: {
+    images: [],
+  },
 };
 
 export default function RootLayout({
